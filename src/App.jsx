5a1c140573983020a57f226e7294d3a62d9aaa3d
Mohamed Essam <109111496+m-essam-s@users.noreply.github.com>
2024-09-04 19:06:00 +0300

    will done

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './components/NavBar';
+import Main from './components/Main';
+import Footer from './components/Footer';
+function App() {
+
+  return (
+    <>
+      <NavBar />
+      <Main />
+      <Footer />
+    </>
+  )
+}
+
+export default App
